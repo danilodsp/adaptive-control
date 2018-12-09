6d1746001d9ce1e242c5ab4f4e9f48d4edbc8941
Danilo <danilopena.ba@gmail.com>
2018-12-09 09:21:31 -0300

    Modified some parameters

--- code/DMARC.m
+++ code/DMARC.m
@@ -8,7 +8,7 @@ close all
 clc
 
 h = 0.0001; 
-t = 0:h:20; 
+t = 0:h:10; 
 n = max(size(t)); 
 
 %% Initialization
@@ -19,10 +19,10 @@ ym_o =  zeros(1,n-1);
 dy_o = zeros(1,n-1);
 y_o = zeros(1,n-1);
 eo_o = zeros(1,n-1);
-dteta1_o = zeros(1,n-1);
-teta1_o = zeros(1,n-1);
-dteta2_o = zeros(1,n-1);
-teta2_o = zeros(1,n-1);
+dtheta1_o = zeros(1,n-1);
+theta1_o = zeros(1,n-1);
+dtheta2_o = zeros(1,n-1);
+theta2_o = zeros(1,n-1);
 u_o = zeros(1,n-1);
 
 % Variables
@@ -33,17 +33,15 @@ y = 0.5;
 eo = 0;
 am = 1;
 km = 1;
-ap = -1.1;
+ap = -1;
 kp = 1;
-teta1 = 0;
-teta2 = 0;
-teta1barra = 2.3;
-teta2barra = 1.3;
-%teta1estrela = -2;
-%teta2estrela = 1;
-sigma = 0.9;
-L = 0.1;
-mi = 1;
+theta1 = 0;
+theta2 = 0;
+theta1_estimated = 2.3;
+theta2_estimated = 1.3;
+sigma = 1;
+L = 0.000005;
+mi = 0;
 
 %% Loops
 
@@ -61,38 +59,37 @@ for  k=1:n
     % Plant
     dy = - ap*y + kp*u;
     dy_o(k) = dy;
-        
+
     y = dy*h + y;
     y_o(k) = y;
-    
+
     % Control Law
-    u = teta1*y + teta2*r;
+    u = theta1*y + theta2*r;
     u_o(k) = u;
-    
+
     % Error
     eo = y - ym;
     eo_o(k) = eo;  
-   
+
     % Controller parameters
-    mi = 1- exp(-((eo*eo)/L));
+    mi = exp(-((eo*eo)/L));
     mi_o(k) = mi;
-    %mi=1;
+    %mi=0;
     
-    if mi<0.1 && mi>=1 % MRAC
-        %dteta1 = -(sigma/mi)*teta1 - (sigma/mi)*teta1barra*sign(eo*y);
-        %dteta2 = -(sigma/mi)*teta2 - (sigma/mi)*teta2barra*sign(eo*r);
-        dteta1 = -sigma*teta1 - sigma*teta1barra*sign(eo*y);
-        dteta2 = -sigma*teta2 - sigma*teta2barra*sign(eo*r);
-        teta1 = dteta1*h + teta1;
-        teta2 = dteta2*h + teta2;
-    else  % VS-MRAC
-        teta1 = - teta1barra*sign(eo*y);
-        teta2 = -teta2barra*sign(eo*r);
-    end 
-    
-    teta1_o(k) = teta1;
-    teta2_o(k) = teta2;
-    
+        if mi>0 % Dual Mode
+            dtheta1 = (-sigma*theta1 - sigma*theta1_estimated*sign(eo*y))/mi;
+            dtheta2 = (-sigma*theta2 - sigma*theta2_estimated*sign(eo*r))/mi;
+
+            theta1 = dtheta1*h + theta1;
+            theta2 = dtheta2*h + theta2;
+        else  % VS-MRAC
+            theta1 = - theta1_estimated*sign(eo*y);
+            theta2 = - theta2_estimated*sign(eo*r);
+        end 
+
+    theta1_o(k) = theta1;
+    theta2_o(k) = theta2;
+
     t_o(k) = k*h;
 end
 
@@ -102,7 +99,7 @@ figure (1)
 plot(t_o,y_o,t_o,ym_o)
 
 figure (2)
-plot(t_o,teta1_o)
+plot(t_o,theta1_o)
 
 %figure (3)
 %plot(t_o,teta2_o)
